fb745e86d47edc8d46501f2147dd6fbb54c49294
hoangminhbao8102 <90952548+hoangminhbao8102@users.noreply.github.com>
2023-05-05 16:21:52 +0700

    main

---
+++ WebHotelManagementSystem/Pages/Log_in.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHotelManagementSystem.Pages
+{
+    public class Log_inModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebHotelManagementSystem/Pages/Sign_up.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHotelManagementSystem.Pages
+{
+    public class Sign_upModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
